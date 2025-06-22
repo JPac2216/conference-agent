fcf82d48aa436f3f3ce86144b613fab95a259c02
JPac2216 <jakepaccione@gmail.com>
2025-06-22 14:34:59 -0400

    Added search tool, fixed bugs

--- agent.py
+++ agent.py
@@ -21,6 +21,8 @@ from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage, To
 from operator import add as add_messages
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.tools import tool
+import requests
+import json
 
 load_dotenv()
 
@@ -44,8 +46,38 @@ SCORE: {query_results['distances'][0][i]}
         return result
     else:
         return "I found no relevant information in the APHA 2025 Session schedule."
-    
-tools = [retriever_tool]
+
+@tool    
+def linkedin_search_tool(name: str, title: str, institution: str) -> str:
+    """This tool retrieves a presenter's LinkedIn profile (if available) given their full name, professional title, and institution.
+    Args:
+    name: Presenter's full name given by the database query
+    title: Presenter's company title given by the database query
+    institution: Presenter's institution given by the database query
+    """
+
+    url = "https://api.tavily.com/search"
+
+    payload = {
+        "query": f"Find {name}'s LinkedIn profile, they are the {title} at {institution}."
+    }
+    headers = {
+        "Authorization": f"Bearer {os.environ['TAVILY_API_KEY']}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    #print(response.text)
+    dict = json.loads(response.text)
+    raw_url = dict["results"][0]["url"]
+
+    if "/posts/" in raw_url or "/activity/" in raw_url:
+        profile = raw_url.split("linkedin.com/")[1].split("/")[1].split("_")[0]
+        return f"https://www.linkedin.com/in/{profile}"
+    return raw_url
+
+tools = [retriever_tool, linkedin_search_tool]
 tools_dict = {our_tool.name: our_tool for our_tool in tools}
 
 # Allow the LLM to use the tools
@@ -57,7 +89,9 @@ class AgentState(TypedDict):
     
 system_prompt = """
 You are an intelligent AI assistant who answers questions about public health conferences and is able to help users plan sessions based on the 'apha2025_sessions.csv' file loaded into your knowledge base.
-Use the retriever tool available to answer the user's latest question regarding all networking, happy hours and social events within this program. When you need to search the database, call retriever_tool with roughly 3–5 **keywords**, not full sentences.
+Always focus your response **only on the user's most recent message**. Ignore earlier user questions unless explicitly referenced.
+
+Use the retriever tool available to answer the user's latest question regarding all events within the program. When you need to search the database, call retriever_tool with roughly 3–5 **keywords**, not full sentences.
 
 Example: Say the user is asking for conferences about COVID on March 22:
 
@@ -70,7 +104,20 @@ Summarize the event for the user with relevent info you find from the tool, and 
 Please provide dates, times (in standard time NOT military time) and locations. Please include special interest groups.
 Include if the user is able to preregister for the event (based on the TRUE/FALSE) data.
 
-Additionally include the speakers, which may be provided either in the 'Presenters' key or in the description itself.
+Additionally include the speakers, which may be provided either in the 'Presenters' key or in the description itself. 
+For EACH speaker included find their LinkedIn profiles by using the LinkedIn search tool and feed it each presenter's full name, title, and institution based on the info given to you from the retriever tool.
+
+    Example: Say the retriever tool gives you:
+
+    Presenters: Jake Paccione
+    Professional Titles: Undergraduate Research Assistant
+    Institutions: Stevens Institute of Technology
+
+    Use the LinkedIn Search Tool:
+
+    { "name": "linkedin_search_tool",
+    "args": { "name": "Jake Paccione", "title": "Undergraduate Research Assistant", "institution": "Stevens Institute of Technology" }
+    }
 
 If you need to look up some information before asking a follow up question, you are allowed to do that!
 Please always cite exactly where you got your answer.
@@ -81,10 +128,10 @@ def call_llm(state: AgentState) -> AgentState:
     """Function to call the LLM with the current state"""
     messages = list(state['messages'])
 
-    messages = [SystemMessage(content=system_prompt)] + messages
+    model_messages = [SystemMessage(content=system_prompt)] + messages
 
-    message = llm.invoke(messages)
-    return {'messages': messages + [message]}
+    message = llm.invoke(model_messages)
+    return {'messages': state['messages'] + [message]}
 
 # Retrieval agent
 def call_tools(state: AgentState) -> AgentState:
@@ -92,7 +139,7 @@ def call_tools(state: AgentState) -> AgentState:
     tool_calls = state['messages'][-1].tool_calls
     results = []
     for t in tool_calls:
-        print(f"Calling Tool: {t['name']} with query: {t['args'].get('query', 'No query provided')}")
+        print(f"Calling Tool: {t['name']} with query: {t['args']}")
 
         # Checking tool validity
         if not t['name'] in tools_dict:
@@ -100,7 +147,7 @@ def call_tools(state: AgentState) -> AgentState:
             result = "Incorrect Tool Name, Please Retry and Select tool from List of Available tools."
 
         else:
-            result = tools_dict[t['name']].invoke(t['args'].get('query', ''))
+            result = tools_dict[t['name']].invoke(t['args'])
             print(f"Result length: {len(str(result))}")
 
         results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
